65da5eae2e0492ead5a57bb7b48ad5e9bed49938
Emre Yasa <emreyasa86@gmail.com>
2021-04-15 04:27:51 -0700

    Add XMLEncoding solution

--- Moderate/11.DivingBoard.playground/Contents.swift
+++ Moderate/11.DivingBoard.playground/Contents.swift
@@ -1,3 +1,47 @@
 import UIKit
 
-var str = "Hello, playground"
+/*
+ You are placing a diving board by placing a bunch of planks of wood end to end.
+ There are 2 types of planks, one of lenght shorter and one of length longer. You must use exactyly K planks of wood.
+ Write a method to generate all possible lengths for the diving board.
+ */
+
+
+//Recursive solutions
+func allLengths(_ k:Int, _ shorter:Int, _ longer:Int) -> Set<Int> {
+    let lengths = Set<Int>()
+    getAllLengths(k, 0, shorter, longer, lengths)
+    return lengths
+}
+
+func getAllLengths(_ k:Int,_ total:Int, _ shorter:Int, _ longer:Int, _ lengths:Set<Int>) {
+    var lengths:Set<Int> = lengths
+    if k == 0 {
+        lengths.insert(total)
+        return
+    }
+    
+    getAllLengths(k-1, total+shorter, shorter, longer, lengths)
+    getAllLengths(k-1, total+longer, shorter, longer, lengths)
+}
+
+//This algo takes O(2^K) time, since there are 2 choices at recursive call and we recurse to a depth of K.
+
+
+//Optimal solutions
+/*
+ We don't actually need to go through all arrangements of planks.
+ We just need to go through all unique sets of K planks (sets, not orders!).
+ There are only K ways of picking K planks
+ if we only have two possible types: {O of type A, K of type B}, {1 of type A, K-1 of type B}, {2 of type A, K-2 of type B}, ...
+ */
+
+func allLengths2(_ k:Int, _ shorter:Int, _ longer:Int) -> Set<Int> {
+    var lengths = Set<Int>()
+    for nShorter in 0...k {
+        let nLonger = k - nShorter
+        let length = nShorter * shorter + nLonger * longer
+        lengths.insert(length)
+    }
+    return lengths
+}
---
+++ Moderate/12.XMLEncoding.playground/Contents.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+
+func encode(xml: String) -> String {
+    guard let doc = try? XMLDocument(xmlString: xml), let root = doc.rootElement() else {
+        return ""
+    }
+     
+    return encode(node: root)
+}
+
+private let tags:[String:String] = [
+    "family": "1"
+    "person": "2"
+    "firstName" : "3"
+    "lastName": "4"
+    "state": "5"
+]
+
+
+private func encode(node:XMLNode) -> String {
+    guard let element = node as? XMLElement else {
+        return node.stringValue ?? ""
+    }
+    
+    guard let nae = element.name, let tag = tags[name] else { return "" }
+    let attributes = (element.attributes ?? []).reduce(into: "") { memo, attr in
+            let key = attr.name ?? ""
+            let value = attr.stringValue ?? ""
+
+            if let tag = tags[key] {
+                memo += "\(tag) \(value) "
+            }
+        }
+
+        let children = element.children ?? []
+
+        var result = "\(tag) \(attributes)0"
+        for child in children {
+            result += " " + encode(node: child)
+        }
+        result += " 0"
+
+        return result
+}
