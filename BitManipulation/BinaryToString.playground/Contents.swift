a2a8238be7f29faaf2e95bde98e43963fb08499e
Emre Yasa <31274880+yasaemre@users.noreply.github.com>
2022-03-05 10:04:15 -0800

    Add question

--- BitManipulation/BinaryToString.playground/Contents.swift
+++ BitManipulation/BinaryToString.playground/Contents.swift
@@ -1,6 +1,9 @@
 import UIKit
 
-var str = "Hello, playground"
+/*
+Given a real number between O and 1 (e.g., 0.72) that is passed in as a double, print the binary representation.
+If the number cannot be represented accurately in binary with at most 32 characters, print "ERROR:'
+*/
 
 func binaryToString(_ num:Double) -> String {
     if num >= 1 || num <= 0 {
