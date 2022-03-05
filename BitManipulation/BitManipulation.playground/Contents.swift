4fec685ecfafe98edbc5f022723a0541424ae787
Emre Yasa <31274880+yasaemre@users.noreply.github.com>
2022-03-05 09:52:29 -0800

    Add comments

--- BitManipulation/BitManipulation.playground/Contents.swift
+++ BitManipulation/BitManipulation.playground/Contents.swift
@@ -1,17 +1,29 @@
 import UIKit
 
-var str = "Hello, playground"
+//This method shifts 1 over by i bits, creating a value that looks like 00010000. 
+//By performing an AND with num, we clear all bits other than the bit at bit i. 
+//Finally, we compare that to 0. If that new value is not zero, then bit i must have a 1. Otherwise, bit i is a 0.
 func getBit(_ num:Int, _ i: Int) -> Bool {
     return ((num & (i << i)) != 0)
 }
 
+//Set Bit shifts 1 over byi bits, creating a value like 00010000. By performing an OR with num, only the
+//value at bit i will change. All other bits of the mask are zero and will not affect num
 func setBit(_ num: Int, _ i:Int) -> Int {
     return num | (1 << i)
 }
+
+//This method operates in almost the reverse of setBit. 
+//First, we create a number like 11101111 by creating the reverse of it (00010000) and negating it. 
+//Then, we perform an AND with num. This will clear the ith bit and leave the remainder unchanged
 func clearBit(_ num: Int, _ i: Int) -> Int {
     var mask:Int = ~(1 << i)
     return num & mask
 }
+
+//To set the ith bit to a valuev, we first clear the bit at position i by using a mask that looks like 11101111. 
+//Then, we shift the intended value,v, left by i bits. This will create a number with bit i equal tov and all other bits equal to 0. 
+//Finally, we OR these two numbers, updating the ith bit ifv is 1 and leaving it as 0 otherwise.
 func updateBit(_ num: Int, _ i:Int, _ isBit1: Bool) ->Int {
     var value = isBit1 ? 1 : 0
     var mask = ~(1 << i)
