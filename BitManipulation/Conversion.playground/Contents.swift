fedf87a8eafba1c099d8a1a6c172242d1fdf4627
Emre Yasa <31274880+yasaemre@users.noreply.github.com>
2022-03-05 10:10:11 -0800

    Add question

--- BitManipulation/Conversion.playground/Contents.swift
+++ BitManipulation/Conversion.playground/Contents.swift
@@ -1,6 +1,12 @@
 import UIKit
 
-var str = "Hello, playground"
+/*
+Write a function to determine the number of bits you would need to flip to convert
+integer A to integer B.
+EXAMPLE
+Input: 29 (or: 11101), 15 (or: 01111)
+Output: 2
+*/
 
 func convert(_ n:Int, _ m:Int) -> Int {
     var count = 0
